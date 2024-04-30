b2feccd1f9117191f7ca04ebd6032c942bc2fcb4
onigbajjmo <onigbajumogbenga@gmail.com>
2024-04-30 11:59:10 +0100

    frontend setup

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      Ehizuahub
+    <div className="flex justify-center items-center h-[100vh]">
+      
+      <Image src='/logo.png' width={400} height={80} />
     </div>
   );
 }
