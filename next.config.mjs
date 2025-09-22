00190c45b5a6f313d0d3362ee77f11a5ecd2ad7d
onigbajumo <onigbajumogbenga@gmail.com>
2025-09-22 06:00:35 -0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        
-        {
-          protocol: 'https',
-          hostname: 'res.cloudinary.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    }
+  swcMinify: false,
   };
   
   export default nextConfig;
