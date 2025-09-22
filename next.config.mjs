c53944072dba1011029cd5123363618326649b7d
onigbajumo <onigbajumogbenga@gmail.com>
2025-09-22 05:29:53 -0700

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
-
-export const config = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     images: {
       remotePatterns: [
         
@@ -10,10 +10,8 @@ export const config = {
           pathname: '/**',
         },
       ],
-    },
-    api: {
-      bodyParser: false,
-    },
+    }
   };
   
+  export default nextConfig;
   
